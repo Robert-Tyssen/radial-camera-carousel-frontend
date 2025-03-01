490ff3f02365068175d1ea7c492ba4754d6deba3
Robert Tyssen <rob.tyssen@gmail.com>
2025-02-28 19:51:48 -0500

    Added material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Button, TextField, Typography } from '@mui/material'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App: React.FC = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box sx={{ display: 'flex', justifyContent: 'center', bgcolor: 'background.default' }}>
+
+        <Box sx={{ maxWidth: 'md', px: 4, pt: 8 }}>
+
+          {/* Main title */}
+          <Typography variant="h3">Placeholder Title</Typography>
+
+          {/* Endpoint text input */}
+          <TextField variant='outlined' label='Web Server Address' defaultValue={'http://localhost:0000/'}/>
+          <Button variant='contained' size='large'>Check</Button>
+
+
+        </Box>
+
+
+      </Box>
     </>
   )
 }
