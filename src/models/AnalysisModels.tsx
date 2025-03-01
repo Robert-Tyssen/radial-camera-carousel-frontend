80bf13a4b09e6f97d20fb65869880c481a7a93d5
Robert Tyssen <rob.tyssen@gmail.com>
2025-03-01 13:03:31 -0500

    Added analysis models

---
+++ src/models/AnalysisModels.tsx
@@ -0,0 +1,40 @@
+import { z } from "zod"
+
+// Overall analysis status data model
+export type AnalysisStatus = {
+  inProgress: boolean,
+  analysisTasks: AnalysisTaskStatus[],
+}
+
+// Analysis task data model 
+export type AnalysisTaskStatus = {
+  photoId: number,
+  cameraId: number,
+  status: string,
+  timeRemaining: number,
+}
+
+
+// Schema for analysis tasks returned from the server,
+// with transformation to the associated data model
+export const AnalysisTaskResponseSchema = z.object({
+  photo_id: z.number(),
+  camera_id: z.number(),
+  status: z.string(),
+  time_remaining: z.number(),
+}).transform((data): AnalysisTaskStatus => ({
+  photoId: data.photo_id,
+  cameraId: data.camera_id,
+  status: data.status,
+  timeRemaining: data.time_remaining
+}));
+
+// Schema for the overall analysis status returned from the server,
+// with transformations to the associated data model
+export const AnalysisResponseSchema = z.object({
+  analysis_in_progress: z.boolean(),
+  analysis_tasks: z.array(AnalysisTaskResponseSchema)
+}).transform((data): AnalysisStatus => ({
+  inProgress: data.analysis_in_progress,
+  analysisTasks: data.analysis_tasks
+}));
